ad7ce4b4bfc4873a6be2dae9d2afe629585172fc
Fireez30 <32301553+Fireez30@users.noreply.github.com>
2017-12-08 16:47:29 +0100

    Add files via upload

--- KnowledgeBase.java
+++ KnowledgeBase.java
@@ -79,25 +79,24 @@ public class KnowledgeBase {
 		}
 	}
 
-	public boolean BackwardChaining(Atom q,ArrayList<Atom> l) {
+	public boolean BackwardChaining(Atom q,ArrayList<Atom> sup) {
 		if (facts.belongsAtom(q)) {return true;}
 		boolean test = false;
 		for(int i=0;i < rules.size();i++) {
 			for(int j=0; j < rules.getRule(i).getHypothesis().size(); j++) {
-				if (l.contains(rules.getRule(i).getHypothesis().get(j))) {test = true;}
+				if (sup.contains(rules.getRule(i).getHypothesis().get(j))) {test = true;}
 			}
 			if (test == false) {
 				int k=1;
-				l.add(rules.getRule(i).getConclusion());
-				while (k<rules.getRule(i).getHypothesis().size() && BackwardChaining(rules.getRule(i).getHypothesis().get(k),l)) {
-					l.remove(rules.getRule(i).getConclusion());
+				if (!sup.contains(q)) {sup.add(q);}
+				while (k<rules.getRule(i).getHypothesis().size() && !sup.contains(rules.getRule(i).getHypothesis().get(k)) && BackwardChaining(rules.getRule(i).getHypothesis().get(k),sup)) {
 					k++;
 				}
-				if (k > rules.getRule(i).getHypothesis().size()) return true;
-				test = false;
+				if (k > rules.getRule(i).getHypothesis().size()) {return true;}
 				}
-			
 			}
+		
+		sup.remove(q);
 		return false;
 	}
 
